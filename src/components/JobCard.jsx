b69c360a0d8b09e0ed4774d5ed7ac2a104a60ac7
Naveed Shaik <shaiknaveed435@gmail.com>
2024-05-07 21:06:53 +0530

    media queries issues fixed

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -6,19 +6,6 @@ import Skeleton from "@mui/material/Skeleton";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 800,
-  bgcolor: "background.paper",
-  // border: "1px solid #000",
-  borderRadius: "10px",
-  boxShadow: 24,
-  p: 4,
-  overflow: "auto",
-};
 const JobCard = ({ obj, isLoading }) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
